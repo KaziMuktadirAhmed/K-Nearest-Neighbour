67243e92c501f8e5d8e4015207c88965aa34e59a
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2022-03-22 15:28:42 +0600

    Initial commit

---
+++ src/MainPackage/Driver.java
@@ -0,0 +1,11 @@
+package MainPackage;
+
+public class Driver {
+    public void  run () {
+        System.out.println("Hello");
+    }
+
+    public void test () {
+
+    }
+}
---
+++ src/MainPackage/MainClass.java
@@ -0,0 +1,8 @@
+package MainPackage;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+        driver.run();
+    }
+}
