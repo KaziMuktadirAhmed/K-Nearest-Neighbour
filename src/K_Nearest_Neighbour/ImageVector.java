c0ea9af72d76c6a0c591e24b90a8d7d668e115c0
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2022-03-23 16:19:21 +0600

    Read image from directory

---
+++ src/K_Nearest_Neighbour/ImageVector.java
@@ -0,0 +1,41 @@
+package K_Nearest_Neighbour;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ImageVector {
+    private String ImageType;
+    private final File ImageFile;
+    private ArrayList<Integer> Vector = new ArrayList<>();
+
+    public ImageVector (File ImageFile, String ImageType) throws IOException {
+        this.ImageFile = ImageFile;
+        this.ImageType = ImageType;
+        convertToImageVector();
+    }
+
+    public void setImageType (String ImageType) {
+        this.ImageType = ImageType;
+    }
+
+    public String Type () {
+        return this.ImageType;
+    }
+
+    private void convertToImageVector() throws IOException {
+        BufferedImage image = ImageIO.read(ImageFile);
+
+        for (int i=0; i<image.getHeight(); i++) {
+            for (int j=0; j<image.getWidth(); j++) {
+                Color pixel = new Color(image.getRGB(j, i));
+                this.Vector.add(pixel.getRed());
+                this.Vector.add(pixel.getGreen());
+                this.Vector.add(pixel.getBlue());
+            }
+        }
+    }
+}
--- src/K_Nearest_Neighbour/KNN_Algorithm.java
+++ src/K_Nearest_Neighbour/KNN_Algorithm.java
@@ -1,4 +1,15 @@
 package K_Nearest_Neighbour;
 
+import java.util.ArrayList;
+
 public class KNN_Algorithm {
+    private ArrayList<ImageVector> datasetType1 = new ArrayList<>();
+    private ArrayList<ImageVector> datasetType2 = new ArrayList<>();
+
+    public KNN_Algorithm (ArrayList<ImageVector> datasetType1, ArrayList<ImageVector> datasetType2) {
+        this.datasetType1 = datasetType1;
+        this.datasetType2 = datasetType2;
+    }
+
+
 }
