3760d0d2d933d98dc4eb4842dc5a5a8a6c33a87d
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2022-03-23 13:19:03 +0600

    starting to do something

--- src/ImageDataset/Dataset.java
+++ src/ImageDataset/Dataset.java
@@ -1,5 +1,27 @@
 package ImageDataset;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Dataset {
-    
+    private String filePath;
+    private ArrayList<File> ImageSet = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> TrainingImagePoints = new ArrayList();
+
+    public Dataset(String filePath) {
+        this.filePath = filePath;
+        readImageFromFile();
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+        readImageFromFile();
+    }
+
+    private void readImageFromFile () {
+        ImageSet.addAll(Arrays.asList(Objects.requireNonNull(new File(filePath).listFiles())));
+
+    }
 }
