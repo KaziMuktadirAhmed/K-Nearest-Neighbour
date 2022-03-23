c88ae41f2d46f28b1c90bb2f8a1e08e99234dcb2
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2022-03-23 17:44:55 +0600

    Read image from directory

--- src/K_Nearest_Neighbour/KNN_Algorithm.java
+++ src/K_Nearest_Neighbour/KNN_Algorithm.java
@@ -2,14 +2,32 @@ package K_Nearest_Neighbour;
 
 import java.util.ArrayList;
 
+
+class Distance_Pair {
+    Double distance;
+    ImageVector image;
+
+    public Distance_Pair (Double distance, ImageVector image) {
+        this.distance = distance;
+        this.image = image;
+    }
+}
+
 public class KNN_Algorithm {
-    private ArrayList<ImageVector> datasetType1 = new ArrayList<>();
-    private ArrayList<ImageVector> datasetType2 = new ArrayList<>();
+    private ArrayList<ImageVector> datasetType1;
+    private ArrayList<ImageVector> datasetType2;
+
+    private ArrayList<Distance_Pair> distance_pairs = new ArrayList<>();
 
     public KNN_Algorithm (ArrayList<ImageVector> datasetType1, ArrayList<ImageVector> datasetType2) {
         this.datasetType1 = datasetType1;
         this.datasetType2 = datasetType2;
     }
+
+    public void CalculateDistance () {
+
+    }
+
 
 
 }
