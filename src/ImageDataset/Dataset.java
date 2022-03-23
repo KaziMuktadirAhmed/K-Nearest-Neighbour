c5d466f04c7572299e82b2a88976517daec6f941
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2022-03-23 13:40:29 +0600

    Read image from directory

--- src/ImageDataset/Dataset.java
+++ src/ImageDataset/Dataset.java
@@ -1,6 +1,10 @@
 package ImageDataset;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
@@ -10,18 +14,33 @@ public class Dataset {
     private ArrayList<File> ImageSet = new ArrayList<>();
     private ArrayList<ArrayList<Integer>> TrainingImagePoints = new ArrayList();
 
-    public Dataset(String filePath) {
+    public Dataset(String filePath) throws IOException {
         this.filePath = filePath;
         readImageFromFile();
     }
 
-    public void setFilePath(String filePath) {
+    public void setFilePath(String filePath) throws IOException {
         this.filePath = filePath;
         readImageFromFile();
     }
 
-    private void readImageFromFile () {
+    private void readImageFromFile () throws IOException {
         ImageSet.addAll(Arrays.asList(Objects.requireNonNull(new File(filePath).listFiles())));
 
+        for (File imageFile : ImageSet) {
+            BufferedImage image = ImageIO.read(imageFile);
+            ArrayList<Integer> ImageVector = new ArrayList<>();
+
+            for (int i=0; i<image.getHeight(); i++) {
+                for (int j=0; j<image.getWidth(); j++) {
+                    Color pixel = new Color(image.getRGB(j, i));
+                    ImageVector.add(pixel.getRed());
+                    ImageVector.add(pixel.getGreen());
+                    ImageVector.add(pixel.getBlue());
+                }
+            }
+
+            TrainingImagePoints.add(ImageVector);
+        }
     }
 }
